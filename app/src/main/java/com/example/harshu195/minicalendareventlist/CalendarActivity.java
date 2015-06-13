f337946fcaf3ee04fbb46158fdd124284aeb6d65
Harsh Patel <harshu195@gmail.com>
2015-06-13 00:44:19 -0400

    Few Changes

--- app/src/main/java/com/example/harshu195/minicalendareventlist/CalendarActivity.java
+++ app/src/main/java/com/example/harshu195/minicalendareventlist/CalendarActivity.java
@@ -14,12 +14,14 @@ import java.util.Date;
 import java.util.List;
 
 public class CalendarActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        String[] projection = new String[] { CalendarContract.Events.CALENDAR_ID, CalendarContract.Events.TITLE, CalendarContract.Events.DESCRIPTION, CalendarContract.Events.DTSTART, CalendarContract.Events.DTEND, CalendarContract.Events.ALL_DAY, CalendarContract.Events.EVENT_LOCATION };
+        String[] projection = new String[] {
+                CalendarContract.Events.CALENDAR_ID, CalendarContract.Events.TITLE, CalendarContract.Events.DESCRIPTION,
+                CalendarContract.Events.DTSTART, CalendarContract.Events.DTEND, CalendarContract.Events.ALL_DAY,
+                CalendarContract.Events.EVENT_LOCATION };
 
         Calendar startTime = Calendar.getInstance();
         startTime.set(2015,5,5,0,0);
@@ -29,21 +31,24 @@ public class CalendarActivity extends Activity {
 
         // the range is June 5th to June 9
 
-        String selection = "(( " + CalendarContract.Events.DTSTART + " >= " + startTime.getTimeInMillis() + " ) AND ( " + CalendarContract.Events.DTSTART + " <= " + endTime.getTimeInMillis() + " ))";
+        String selection = "(( " + CalendarContract.Events.DTSTART + " >= " + startTime.getTimeInMillis() + " ) AND" +
+                " ( " + CalendarContract.Events.DTSTART + " <= " + endTime.getTimeInMillis() + " ))";
 
         Cursor cursor = this.getBaseContext().getContentResolver().query( CalendarContract.Events.CONTENT_URI, projection, selection, null, null );
 
         // output the events
-        /*  ListView lvEvents = (ListView)findViewById(R.id.lvEvents);
-            CalendarEventsAdapter calendarEventsAdapter = new CalendarEventsAdapter(CalendarActivity.this,cursor);
-            lvEvents.setAdapter(calendarEventsAdapter);
+        /*
+        // ListView lvEvents = (ListView)findViewById(R.id.lvEvents);
+        // CalendarEventsAdapter calendarEventsAdapter = new CalendarEventsAdapter(CalendarActivity.this,cursor);
+        // lvEvents.setAdapter(calendarEventsAdapter);
         */
+
         TextView tvres = (TextView) findViewById(R.id.tvres);
         if (cursor.moveToFirst()) {
             do {
-                tvres.append( "\n" +"\n" + "Title: " + " " + cursor.getString(1) +  "\n" +"\n" +  " Start-Time: " + " " +  (new Date(cursor.getLong(3))).toString()+  "\n" +"\n" +  " End-Time: " + " " +  (new Date(cursor.getLong(4))).toString());
-                //Toast.makeText(this.getApplicationContext(), "Title: " + cursor.getString(1) + " Start-Time: " + (new Date(cursor.getLong(3))).toString(), Toast.LENGTH_LONG).show();
-                Log.i("TEst","1");
+                tvres.append("\n" + "Title: " + " " + cursor.getString(1)+"\n"
+                        +"\n" +  " Start-Time: " + " " +  (new Date(cursor.getLong(3))).toString()+"\n"
+                        +"\n" +  " End-Time: " + " " +  (new Date(cursor.getLong(4))).toString()+"\n");
             } while ( cursor.moveToNext());
         }
     }
